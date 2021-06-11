55777dd3cffdb3a0d46c18b04b3ea09eb93bb12d
armankhanx14 <arman@development.europe-west2-c.c.model-journal-311913.internal>
2021-06-11 17:00:40 +0000

    small changes made

--- jenkins/install-requirements.sh
+++ jenkins/install-requirements.sh
@@ -13,6 +13,11 @@ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | 
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Install Ansible
+sudo apt update 
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
 
 # Docker login
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
