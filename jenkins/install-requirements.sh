255ce0f5add74f5702890bff2297c0aa3a962fc4
armankhanx14 <arman@development.europe-west2-c.c.model-journal-311913.internal>
2021-06-11 11:11:23 +0000

    changes made to install-requirements.sh

--- jenkins/install-requirements.sh
+++ jenkins/install-requirements.sh
@@ -13,6 +13,9 @@ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | 
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+
 # Docker login
 docker login --username $DOCKER_USERNAME --password $DOKCER_PASSWORD
 
+echo Username: $DOCKER_USERNAME
+echo Password: $DOKCER_PASSWORD
