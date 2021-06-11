5146c5164e4702c8ea13d10927b978f2e963c6a7
armankhanx14 <arman@development.europe-west2-c.c.model-journal-311913.internal>
2021-06-11 10:36:33 +0000

    adding build, push, install stages

---
+++ jenkins/install-requirements.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y curl jq
+
+#i Install Docker
+curl https://get.docker.com | sudo bash
+sudo usermog -aG docker jenkins
+
+
+# Install Docker Compose
+
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Docker login
+docker login --username $DOCKER_USERNAME --password $DOKCER_PASSWORD
+
