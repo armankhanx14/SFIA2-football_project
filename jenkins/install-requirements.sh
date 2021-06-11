f2d701bbdaabc1b3d2ad5a6feecbad20cb3225cb
armankhanx14 <arman@development.europe-west2-c.c.model-journal-311913.internal>
2021-06-11 17:59:22 +0000

    Added Jenkins folder

---
+++ jenkins/install-requirements.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y curl jq
+
+#i Install Docker
+curl https://get.docker.com | sudo bash
+sudo usermog -aG docker jenkins
+
+
+# Install Docker Compose
+
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install Ansible
+sudo apt update 
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+
+# Docker login
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+
+echo Username: $DOCKER_USERNAME
+echo Password: $DOCKER_PASSWORD
