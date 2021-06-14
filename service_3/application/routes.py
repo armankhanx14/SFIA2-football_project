4a48c9791284e1169d29194016428575336f6440
armankhanx14 <82821737+armankhanx14@users.noreply.github.com>
2021-06-14 14:10:26 +0500

    Feature/ansible nginx (#6)
    * changes made to all servers
    
    * changes made
    
    * final changes made into feature
    
    * changes made for presentation
    
    * final changes
    
    Co-authored-by: arman-khan <akhan11@qa.com>

--- service_3/application/routes.py
+++ service_3/application/routes.py
@@ -4,7 +4,7 @@ import random
 
 @app.route("/team", methods=["GET"])
 def get_team():
-    teams = ["Chelsea", "Totenham", "ManchesterUnited", "Liverpool", "West-Ham"]
+    teams = ["Chelsea", "Totenham", "ManchesterUnited", "Liverpool","West-Ham","Fulham","Leeds"]
     return Response(str(random.choice(teams)), mimetype='text/plain')
 
 
