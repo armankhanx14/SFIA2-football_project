ed7828e84bc0c72984e7924a1faf3e75750f8b4a
arman-khan <akhan11@qa.com>
2021-06-14 08:59:56 +0100

    final changes made into feature

--- service_3/application/routes.py
+++ service_3/application/routes.py
@@ -4,7 +4,7 @@ import random
 
 @app.route("/team", methods=["GET"])
 def get_team():
-    teams = ["Chelsea", "Totenham", "ManchesterUnited", "Liverpool"]
+    teams = ["Chelsea", "Totenham", "ManchesterUnited", "Liverpool","West-Ham"]
     return Response(str(random.choice(teams)), mimetype='text/plain')
 
 
